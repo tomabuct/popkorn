26db3fe34bc2abfa7c8e62d2cd4f45681e40d985
Peter Xiao <phx@andrew.cmu.edu>
2013-01-19 11:21:09 -0500

    Questionable tokbox in.

--- public/pop.js
+++ public/pop.js
@@ -1,4 +1,7 @@
 $(document).ready(function() {
+  var apiKey = '22593122';
+  var sessionId = '2_MX4yMjU5MzEyMn4xMjcuMC4wLjF-U2F0IEphbiAxOSAwODoxMjowNiBQU1QgMjAxM34wLjk4Mzc4NzA2fg';
+  var token = 'T1==cGFydG5lcl9pZD0yMjU5MzEyMiZzaWc9ZDZiMzQzNWFmZjU5OWNjYzhmNjNkOWYxMDRjNmZmMGQ0ODY1MTJkMTpzZXNzaW9uX2lkPTJfTVg0eU1qVTVNekV5TW40eE1qY3VNQzR3TGpGLVUyRjBJRXBoYmlBeE9TQXdPRG94TWpvd05pQlFVMVFnTWpBeE0zNHdMams0TXpjNE56QTJmZyZjcmVhdGVfdGltZT0xMzU4NjExOTI5JmV4cGlyZV90aW1lPTEzNTg2OTgzMjkmcm9sZT1wdWJsaXNoZXImbm9uY2U9NDQzOTYyJnNka192ZXJzaW9uPXRiLWRhc2hib2FyZC1qYXZhc2NyaXB0LXYx';
   var client = new Dropbox.Client({
     key: "KGh2bRY1OTA=|uW+0UZq9oX+WVLvRsNCnnIcuEvgOkhOmU0vYFgzhkA==" 
   });
@@ -111,6 +114,43 @@ $(document).ready(function() {
             sent = false;
           }
         });
+
+        TB.setLogLevel(TB.DEBUG);
+        var session = TB.initSession(sessionId);
+        session.addEventListener('sessionConnected', sessionConnectedHandler);
+        session.addEventListener('streamCreated', streamCreatedHandler);
+        session.connect(apiKey, token);
+
+        var publisher;
+
+        function sessionConnectedHandler(event) {
+          console.log("connected!");
+          // Put my webcam in a div
+          var publishProps = {height:240, width:320};
+          publisher = TB.initPublisher(apiKey, 'publisher', publishProps);
+          // Send my stream to the session
+          session.publish(publisher);
+
+          subscribeToStreams(event.streams);
+        }
+        
+        function streamCreatedHandler(event) {
+          subscribeToStreams(event.streams);
+        }
+
+        function subscribeToStreams(streams) {
+          for (var i = 0; i < streams.length; i++) {
+            if (streams[i].connection.connectionId == session.connection.connectionId) {
+              return;
+            }
+
+            var div = document.createElement('div');
+            div.setAttribute('id', 'stream' + streams[i].steamId);
+            document.body.appendChild(div);
+
+            session.subscribe(streams[i], div.id)
+          }
+        }
       });
     });
   }

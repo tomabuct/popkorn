a3c2d89c0d1e1c97cf82c66c63feec6caeacfa66
Tom Abraham <tabraham@andrew.cmu.edu>
2013-01-19 01:53:26 -0500

    fixed pusher push notifications

--- sinatra.rb
+++ sinatra.rb
@@ -1,5 +1,16 @@
 require 'sinatra'
+require 'rubygems'
+require 'hmac-sha2'
 
 get '/' do
   "Hello World!"
+end
+
+post '/pusher/auth' do
+  key = '86baf974dd9fd950a9c8'
+  secret = 'e3fcaaf58db221657bf4'
+  string_to_sign = "#{params[:socket_id]}:#{params[:channel_name]}"
+
+  signature = HMAC::SHA256.hexdigest(secret, string_to_sign)
+  "{\"auth\":\"#{key}:#{signature}\"}"
 end

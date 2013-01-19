d2b4713f90ce3570cc5fce00958542a35c4f1fee
Tom Abraham <tabraham@andrew.cmu.edu>
2013-01-18 22:12:39 -0500

    fix

---
+++ server.rb
@@ -0,0 +1,42 @@
+require 'em-websocket'
+# requires the twitter-stream gem
+require 'twitter/json_stream'
+require 'json'
+
+#
+# broadcast all ruby related tweets to all connected users!
+#
+
+EventMachine.run {
+  @channel = EM::Channel.new
+
+  # @twitter = Twitter::JSONStream.connect(
+  #   :path => '/1/statuses/filter.json?track=ruby',
+  #   :auth => "#{username}:#{password}",
+  #   :ssl => true
+  # )kkkk
+
+  # @twitter.each_item do |status|
+  #   status = JSON.parse(status)
+  #   @channel.push "#{status['user']['screen_name']}: #{status['text']}"
+  # end
+j
+  EventMachine::WebSocket.start(:host => "0.0.0.0", :port => 8080, :debug => true) do |ws|
+
+    ws.onopen {
+      sid = @channel.subscribe { |msg| ws.send msg }
+      @channel.push "#{sid} connected!"
+
+      ws.onmessage { |msg|
+        @channel.push "#{msg}"
+      }
+
+      ws.onclose {
+        @channel.unsubscribe(sid)
+      }
+    }
+
+  end
+
+  puts "Server started"
+}
